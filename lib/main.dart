174064502d7fd5727f500e86d5c2e0970b11c924
nourhaine1 <ayadinourhaine@gmail.com>
2022-07-03 17:40:33 +0200

    add photo and center a title

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
       MaterialApp(
       home: Scaffold(
+          backgroundColor:Colors.black45,
 
         appBar:AppBar(
       title:Text("heeloooooo i am rich "),
@@ -12,10 +13,14 @@ void main() {
 
           ),
         body:Center(
+
+
           child: Image(
+
             image: NetworkImage('http://cdn.thinglink.me/api/image/457231869876895745/1024/10/scaletowidth/0/0/1/1/false/true?wait=true'),
           ),
         )
+
         //Image.network("https://duckduckgo.com/?q=diamond&t=newext&atb=v276-1&iar=images&iax=images&ia=images&iai=http%3A%2F%2Fweknowyourdreams.com%2Fimages%2Fdiamond%2Fdiamond-06.jpg")
       ),
       ),
